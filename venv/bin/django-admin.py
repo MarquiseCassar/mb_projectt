6297f72b3afa9203fea9c296fa8a9b5167d24f47
marquise.cassar <marquise.cassar@newbury.edu>
2018-11-13 15:49:23 -0500

    code complete

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/marquise.cassar/Dropbox/PycharmProjects/mb_projectt/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
